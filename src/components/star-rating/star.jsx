6408189eb4ac5f9084841416c6124b44fd76ba99
madhuravani7 <madhubudige67@gmail.com>
2024-08-05 17:32:12 +1000

    star rating component

--- src/components/star-rating/star.jsx
+++ src/components/star-rating/star.jsx
@@ -1,32 +1,37 @@
 import { useState } from 'react'
 import {FaStar} from 'react-icons/fa'
-
-const Star = ({noOfStars = 5}) => {
+import './star.scss'
+const Star = ({noOfStars = 10}) => {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
 
     function handleClick(getCurrentIndex){
-        console.log(getCurrentIndex)
+        setRating(getCurrentIndex);
     }
 
     function handleMouseEnter(getCurrentIndex){
-        console.log(getCurrentIndex)
+        setHover(getCurrentIndex);
     }
 
-    function handleMouseLeave(getCurrentIndex){
-        console.log(getCurrentIndex)
+    function handleMouseLeave(){
+        setHover(rating);
     }
   return (
-    <div className="star-rating">
+    <div className="star-rating" style={{
+        height: '100vh',
+        width: '100vw'
+
+    }}>
         {
             [...Array(noOfStars)].map((_,index) => {
                 index += 1
 
                 return <FaStar 
                 key={index}
+                className={index <= (hover || rating) ? 'active': 'inactive'}
                 onClick={() => handleClick(index)}
                 onMouseMove={() => handleMouseEnter(index)}
-                onMouseLeave={() => handleMouseLeave(index)}
+                onMouseLeave={() => handleMouseLeave()}
                 size={40}
                 />
             })
